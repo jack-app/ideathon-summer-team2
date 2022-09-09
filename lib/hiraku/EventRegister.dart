04e026c2097b29cf5933d924b41f71fa7fde85d5
hirakuuuu <m.hiraku1129@gmail.com>
2022-09-09 23:22:16 +0900

    [add & fix] 日付入力フォームを作成、レイアウトの修正

--- lib/hiraku/EventRegister.dart
+++ lib/hiraku/EventRegister.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'package:intl/intl.dart';
 
 import 'package:flutter/material.dart';
 
@@ -18,6 +19,21 @@ class EventRegisterWidget extends StatelessWidget {
   }
 }
 
+class FormLabelText extends Container {
+  FormLabelText(String titleText)
+      : super(
+          width: double.infinity,
+          child: Text(
+            titleText,
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 12.0,
+            ),
+          ),
+        );
+}
+
 // リスト一覧画面用Widget
 class EventRegisterPage extends StatefulWidget {
   @override
@@ -28,24 +44,63 @@ class _EventRegisterPageState extends State<EventRegisterPage> {
   // State
   // イベントの名前
   String EventName = '';
+  // イベントの日程
+  var EventDate = DateTime.now();
   // 参加メンバーのリスト
   List<Map> memberList = [];
   // メンバー情報の初期値
   var member = {
     'name': 'test', // 名前
     'payment': 0, // 支払金額
+    'deadline': DateTime.now(), // 期限
   };
 
   // 新規メンバーの値
   var newMemberName = "";
-  var newMemberPayment = 0;
+  var newMemberPaymentStr = "";
+  var newMemberDeadline = DateTime.now();
+
+  // エラー文
+  var FormExceptionText = "";
 
   // テキストフィールドの制御
   var _memberNameController = TextEditingController();
   var _memberPaymentController = TextEditingController();
 
+  // 日付を入力するフォーム
+  _EventDatePicker(BuildContext context) async {
+    final DateTime? datePicked = await showDatePicker(
+        context: context,
+        initialDate: EventDate,
+        firstDate: DateTime(2003),
+        lastDate: DateTime(2023));
+    if (datePicked != null && datePicked != EventDate) {
+      setState(() {
+        EventDate = datePicked;
+      });
+    }
+  }
+
+  // メンバーの
+  _DeadlinePicker(BuildContext context) async {
+    final DateTime? datePicked = await showDatePicker(
+        context: context,
+        initialDate: newMemberDeadline,
+        firstDate: DateTime(2003),
+        lastDate: DateTime(2023));
+    if (datePicked != null && datePicked != newMemberDeadline) {
+      setState(() {
+        newMemberDeadline = datePicked;
+      });
+    }
+  }
+
+  // 日付のフォーマッター
+  DateFormat outputFormat = DateFormat('yyyy-MM-dd');
+
   @override
   Widget build(BuildContext context) {
+    var _size = MediaQuery.of(context).size;
     return Scaffold(
       // AppBarを表示し、タイトルも設定
       appBar: AppBar(
@@ -54,7 +109,7 @@ class _EventRegisterPageState extends State<EventRegisterPage> {
       // ListViewを使いリスト一覧を表示
       body: Column(children: [
         // イベント名の入力フィールド
-        TextField(
+        TextFormField(
           // 自動フォーカス
           autofocus: true,
           // テキスト入力のラベルを設定
@@ -65,6 +120,46 @@ class _EventRegisterPageState extends State<EventRegisterPage> {
             });
           },
         ),
+        const SizedBox(height: 10),
+        FormLabelText('イベントの日程'),
+        Row(
+          children: <Widget>[
+            TextButton(
+                style: TextButton.styleFrom(
+                  primary: Colors.black,
+                  fixedSize: Size(
+                    _size.width, // 画面横いっぱい
+                    10,
+                  ),
+                ),
+                onPressed: () {
+                  _EventDatePicker(context);
+                },
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(outputFormat.format(EventDate),
+                      textAlign: TextAlign.left),
+                )),
+          ],
+        ),
+        Container(
+          height: 1.0,
+          color: Colors.grey,
+        ),
+
+        const SizedBox(height: 15),
+        Container(
+          width: double.infinity,
+          child: Text(
+            'メンバー',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+
         // 確定したメンバーのリスト表示
         ListView.builder(
           // ListViewをColumn内で使うときはこれを書かないとダメらしい
@@ -75,8 +170,11 @@ class _EventRegisterPageState extends State<EventRegisterPage> {
           itemBuilder: (context, index) {
             return Card(
               child: ListTile(
-                title: Text(memberList[index]['name']),
-                subtitle: Text(memberList[index]['payment'].toString()),
+                title: Text(memberList[index]['name'] +
+                    '   ' +
+                    memberList[index]['payment'].toString() +
+                    '円   期限:' +
+                    outputFormat.format(memberList[index]['deadline'])),
 
                 //右側にボタンを配置
                 trailing: Row(
@@ -103,7 +201,7 @@ class _EventRegisterPageState extends State<EventRegisterPage> {
 
         Column(children: <Widget>[
           // テキスト入力
-          TextField(
+          TextFormField(
             // コントローラー
             controller: _memberNameController,
             // 自動フォーカス
@@ -117,7 +215,7 @@ class _EventRegisterPageState extends State<EventRegisterPage> {
             },
           ),
           const SizedBox(height: 8),
-          TextField(
+          TextFormField(
             // コントローラー
             controller: _memberPaymentController,
             // 数字入力指定
@@ -128,9 +226,42 @@ class _EventRegisterPageState extends State<EventRegisterPage> {
             decoration: InputDecoration(labelText: "金額"),
             onChanged: (String value) {
               setState(() {
-                newMemberPayment = int.parse(value);
+                newMemberPaymentStr = value;
               });
             },
+          ),
+          const SizedBox(height: 8),
+          FormLabelText('支払期限'),
+          Row(
+            children: <Widget>[
+              TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.black,
+                    fixedSize: Size(
+                      _size.width, // 画面横いっぱい
+                      10,
+                    ),
+                  ),
+                  onPressed: () {
+                    _DeadlinePicker(context);
+                  },
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(outputFormat.format(newMemberDeadline),
+                        textAlign: TextAlign.left),
+                  )),
+            ],
+          ),
+          Container(
+            height: 1.0,
+            color: Colors.grey,
+          ),
+
+          Text(
+            FormExceptionText,
+            style: TextStyle(
+              color: Colors.red,
+            ),
           ),
         ])
       ]),
@@ -138,21 +269,45 @@ class _EventRegisterPageState extends State<EventRegisterPage> {
       // メンバーを追加するボタン
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            // メンバーの追加
-            memberList.add({
-              'name': newMemberName, // 名前
-              'payment': newMemberPayment, // 支払金額
+          if (newMemberName != "" && newMemberPaymentStr != "") {
+            try {
+              setState(() {
+                // メンバーの追加
+                memberList.add({
+                  'name': newMemberName, // 名前
+                  'payment': int.parse(newMemberPaymentStr), // 支払金額
+                  'deadline': newMemberDeadline,
+                });
+                // 新しいメンバーの値を初期化
+                newMemberName = "";
+                newMemberPaymentStr = "";
+                newMemberDeadline = DateTime.now();
+              });
+              // テキストフィールドの入力を削除
+              _memberNameController.clear();
+              _memberPaymentController.clear();
+            } catch (e) {
+              setState(() {
+                //例外が発生したら実行する処理
+                FormExceptionText = "金額は数字で入力してください";
+              });
+            }
+          } else if (newMemberName != "") {
+            setState(() {
+              //例外が発生したら実行する処理
+              FormExceptionText = "金額が未入力です";
             });
-
-            // 新しいメンバーの値を初期化
-            newMemberName = "";
-            newMemberPayment = 0;
-          });
-
-          // テキストフィールドの入力を削除
-          _memberNameController.clear();
-          _memberPaymentController.clear();
+          } else if (newMemberPaymentStr != "") {
+            setState(() {
+              //例外が発生したら実行する処理
+              FormExceptionText = "名前が未入力です";
+            });
+          } else {
+            setState(() {
+              //例外が発生したら実行する処理
+              FormExceptionText = "名前・金額が未入力です";
+            });
+          }
         },
         child: Icon(Icons.add),
       ),
