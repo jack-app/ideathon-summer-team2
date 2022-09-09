67724726685f081b71974eb3770c1eccc0a49534
keita131 <keita.hiraoka131@gmail.com>
2022-09-09 20:09:48 +0900

    ホーム画面UI仮作成

--- lib/main.dart
+++ lib/main.dart
@@ -72,14 +72,63 @@ class HomePage extends StatelessWidget {
   @override
  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          // ボタンをクリックした時の処理
-          onPressed: () {
-            // "pop"で前の画面に戻る
-            Navigator.of(context).pop();
-          },
-          child: Text('イベント一覧'),
+      body: Container(
+        alignment: Alignment.center,    
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 100,
+              child:Text('イベント一覧',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 36)),
+              
+            ),
+            //イベントリスト
+            Container(
+              height: 400,
+              margin: EdgeInsets.all(40),
+              color: Colors.blue,
+              child:ListView(
+                children: <Widget>[
+                  
+                  Card(
+                    child: ListTile(
+                      title: Text('1/3:ほげほげ'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      title: Text('1/3:ほげほげ'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      title: Text('1/3:ほげほげ'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      title: Text('1/3:ほげほげ'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //新規登録画面
+            Container(
+              margin: EdgeInsets.all(40),
+              child:SizedBox(
+                width: 200, //横幅
+                height: 50, //高さ
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.blue,),
+                  onPressed: () { // ここで遷移先の画面を指定　
+                  },
+                  child: Text('イベント新規登録', style: TextStyle(color: Colors.white)),
+                ),
+              ),
+            ),
+          ]
         ),
       ),
     );
