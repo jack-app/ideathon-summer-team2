9fcea506809e081f6f44177ca620195f63c47aa2
mono <you@example.com>
2022-09-26 11:29:04 +0900

    一覧画面から詳細画面

--- lib/eventlist.dart
+++ lib/eventlist.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import 'main.dart';
+import 'eventdetail.dart';
 
 // リスト一覧画面用Widget
 class EventListPage extends StatelessWidget {
@@ -41,13 +42,14 @@ class EventListPage extends StatelessWidget {
                                 .format(document['date'].toDate())),
                             // 自分の投稿メッセージの場合は削除ボタンを表示
                             trailing: IconButton(
-                              icon: Icon(Icons.delete),
+                              icon: Icon(Icons.info),
                               onPressed: () async {
                                 // 投稿メッセージのドキュメントを削除
-                                await FirebaseFirestore.instance
-                                    .collection('events')
-                                    .doc(document.id)
-                                    .delete();
+                                await Navigator.of(context)
+                                    .push(MaterialPageRoute(builder: (context) {
+                                  // 遷移先の画面としてリスト追加画面を指定
+                                  return EventDetail(eventid: document.id);
+                                }));
                               },
                             )),
                       );
@@ -62,76 +64,5 @@ class EventListPage extends StatelessWidget {
             ),
           )
         ]));
-  }
-}
-
-class TodoAddPage extends StatefulWidget {
-  @override
-  _TodoAddPageState createState() => _TodoAddPageState();
-}
-
-class _TodoAddPageState extends State<TodoAddPage> {
-  // 入力されたテキストをデータとして持つ
-  String _text = '';
-
-  // データを元に表示するWidget
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('リスト追加'),
-      ),
-      body: Container(
-        // 余白を付ける
-        padding: EdgeInsets.all(64),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // 入力されたテキストを表示
-            Text(_text, style: TextStyle(color: Colors.blue)),
-            const SizedBox(height: 8),
-            // テキスト入力
-            TextField(
-              // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
-              onChanged: (String value) {
-                // データが変更したことを知らせる（画面を更新する）
-                setState(() {
-                  // データを変更
-                  _text = value;
-                });
-              },
-            ),
-            const SizedBox(height: 8),
-            Container(
-              // 横幅いっぱいに広げる
-              width: double.infinity,
-              // リスト追加ボタン
-              child: ElevatedButton(
-                onPressed: () {
-                  // "pop"で前の画面に戻る
-                  // "pop"の引数から前の画面にデータを渡す
-                  Navigator.of(context).pop(_text);
-                },
-                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Container(
-              // 横幅いっぱいに広げる
-              width: double.infinity,
-              // キャンセルボタン
-              child: TextButton(
-                // ボタンをクリックした時の処理
-                onPressed: () {
-                  // "pop"で前の画面に戻る
-                  Navigator.of(context).pop();
-                },
-                child: Text('キャンセル'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
