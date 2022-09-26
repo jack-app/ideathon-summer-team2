cea5b0587c39a1cd5e2e8127fb508c68e987b633
keita131 <keita.hiraoka131@gmail.com>
2022-09-26 10:26:38 +0900

    スタート画面とホーム画面

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
+
 void main() => runApp(MyApp()); 
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'お金集めるんです',
+      title: 'マネコレ',
       theme: ThemeData(
-        primarySwatch: Colors.grey,),
+        primarySwatch: Colors.blue,),
       home: TitlePage(),
     );
   }
 }
 
-//タイトル画面      
+//スタート画面      
 class TitlePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -24,113 +26,47 @@ class TitlePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             //タイトル
-          Container(
-            margin: EdgeInsets.symmetric(
-              vertical: 200.0,
-              horizontal: 20.0,),
-            child:Text('集金アプリ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 36)),),
-          //イベント一覧ボタン
-          Container(
-            margin: EdgeInsets.all(40),
-            child:SizedBox(
-              width: 200, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(primary: Colors.grey,),
-                onPressed: () {
-                  Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) {
-                // 遷移先の画面としてリスト追加画面を指定
-                  return HomePage();}),
-                );},
-                child: Text('イベント一覧', style: TextStyle(color: Colors.white)),
-              ),
-            ),
-          ),
-        
-          //イベント登録画面
-          Container(
-            margin: EdgeInsets.all(40),
-            child:SizedBox(
-              width: 200, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(primary: Colors.grey,),
-                onPressed: () {},
-                child: Text('イベント登録', style: TextStyle(color: Colors.white)),
-            ),
-              ),
-            ),
-            ]
-        ),
-      ),
-    ); 
-  }
-}
-
-//ホーム画面
-class HomePage extends StatelessWidget {
-  @override
- Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,    
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
             Container(
-              height: 100,
-              child:Text('イベント一覧',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 36)),
-              
-            ),
-            //イベントリスト
-            Container(
-              height: 400,
-              margin: EdgeInsets.all(40),
-              color: Colors.grey,
-              child:ListView(
-                children: <Widget>[
-                  Card(
-                    child: ListTile(
-                      title: Text('1/3:ほげほげ'),
-                    ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text('1/3:ほげほげ'),
-                    ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text('1/3:ほげほげ'),
-                    ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text('1/3:ほげほげ'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            //新規登録画面
+              margin: EdgeInsets.symmetric(
+                vertical: 200.0,
+                horizontal: 20.0,),
+              child:Text('マネコレ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 36)),),
+            
+            //アカウント作成ボタン
             Container(
               margin: EdgeInsets.all(40),
               child:SizedBox(
                 width: 200, //横幅
                 height: 50, //高さ
                 child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.grey,),
-                  onPressed: () { // ここで遷移先の画面を指定　
-                  },
-                  child: Text('イベント新規登録', style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(primary: Colors.blue,),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) {
+                  // 遷移先の画面としてアカウント作成画面を指定
+                    return CreateAcount();}),
+                  );},
+                  child: Text('アカウント作成', style: TextStyle(color: Colors.white)),
+                ),
+              ),
+            ),
+          
+            //ログイン画面ボタン
+            Container(
+              margin: EdgeInsets.all(40),
+              child:SizedBox(
+                width: 200, 
+                height: 50, 
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.blue,),
+                  onPressed: () {},
+                  child: Text('ログイン', style: TextStyle(color: Colors.white)),
                 ),
               ),
             ),
           ]
         ),
       ),
-    );
+    ); 
   }
 }
